4fd59ae2a2ce177ab80fe85a568956c957c507e2
htrucci <htrucci@gmail.com>
2017-06-18 23:42:14 +0900

    07-10

--- ViewController.swift
+++ ViewController.swift
@@ -8,18 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIWebViewDelegate {
+    @IBOutlet weak var myActivityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var myWebView: UIWebView!
+    @IBOutlet weak var txtUrl: UITextField!
+    
+    func loadWebPage(url: String){
+        let myUrl = NSURL(string: url)
+        let myRequest = NSURLRequest(url: myUrl! as URL )
+        myWebView.loadRequest(myRequest as URLRequest)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myWebView.delegate = self
+        loadWebPage(url: "http://archive.htrucci.com")
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        myActivityIndicator.startAnimating()
+    }
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        myActivityIndicator.stopAnimating()
+    }
 
-
+    @IBAction func btnGotoUrl(_ sender: UIButton) {
+        
+    }
+    @IBAction func btnGoSite1(_ sender: UIButton) {
+        loadWebPage(url: "http://www.naver.com")
+    }
+    @IBAction func btnGoSite2(_ sender: UIButton) {
+        loadWebPage(url: "http://www.daum.net")
+    }
+    @IBAction func btnLoadHtmlString(_ sender: UIButton) {
+        let htmlString = "<h1> HTML STring </h1><p> String 변수를 이용한 웹페이지 </p> <p> <a href=\"http://htrucci.com\">htrucci.com</a>으로 이동 </p>"
+        myWebView.loadHTMLString(htmlString, baseURL: nil)
+    }
+    @IBAction func btnLoadHtmlFile(_ sender: UIButton) {
+        let myHtmlBundle = Bundle.main
+        let filePath = myHtmlBundle.path(forResource: "htmlView", ofType: "html")
+        loadWebPage(url: filePath!)
+    }
+    @IBAction func btnStop(_ sender: UIBarButtonItem) {
+        myActivityIndicator.stopAnimating()
+        myWebView.stopLoading()
+    }
+    @IBAction func btnReload(_ sender: UIBarButtonItem) {
+        myActivityIndicator.stopAnimating()
+        myWebView.reload()
+    }
+    @IBAction func btnGoBack(_ sender: UIBarButtonItem) {
+        myWebView.goBack()
+    }
+    @IBAction func btnGoForward(_ sender: UIBarButtonItem) {
+        myWebView.goForward()
+    }
 }
 
